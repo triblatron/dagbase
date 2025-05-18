cefbb09317e58b7982fff4345cedc81be6b095f0
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2025-05-18 14:28:43 +0100

    refactor:Widget:Put forward decls all under same namespace decl. build:Add Reference to cmake build. refactor:Widget, RootWidget:Rename lookupWidget() to lookup(). - Allows Reference to be generic. feat:Add generic reference typically using Atom as the Identifier and a pointer as the Ref. - Could be further parameterised by the lookup instead of forcing it to be the same type as Ref. test:Add tests for resolve()ing a WidgetRef. test,data:Lookup.lua:Add test data for WidgetRef tests.

---
+++ include/core/Reference.h
@@ -0,0 +1,61 @@
+//
+// Created by Tony Horrobin on 18/05/2025.
+//
+
+#pragma once
+
+#include "config/DagBaseExport.h"
+
+#include <variant>
+#include <string>
+
+namespace dagbase
+{
+    //! Avoid Windows linker barfage by not specifying DAGBASE_API here.
+    template<typename Identifier, typename Ref>
+    class Reference
+    {
+    public:
+        void setId(Identifier id)
+        {
+            _ref = id;
+        }
+
+        void resolve(Ref* lookup)
+        {
+            _ref = lookup->lookup(std::get<Identifier>(_ref));
+        }
+
+        Ref* ref()
+        {
+            if (_ref.index()==1)
+            {
+                return std::get<1>(_ref);
+            }
+
+            return nullptr;
+        }
+
+        std::string toString() const
+        {
+            switch (_ref.index())
+            {
+                case 0:
+                {
+                    if (auto value=std::get<0>(_ref); value.value()!=nullptr)
+                    {
+                        return value.value();
+                    }
+                }
+                    break;
+                case 1:
+                    return "0x" + std::to_string(reinterpret_cast<std::uintptr_t>(std::get<1>(_ref)));
+            }
+
+            return {};
+        }
+    private:
+        using ValueType = std::variant<Identifier, Ref*>;
+        ValueType _ref;
+    };
+}
