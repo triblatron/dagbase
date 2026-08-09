0e84d3a063ea1870a6de023bc17157e5037d3c85
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-08-09 22:30:03 +0100

    build,fix:Prevent Windows barfage by adding export symbol.

--- include/core/SignalPathTable.h
+++ include/core/SignalPathTable.h
@@ -33,7 +33,7 @@ namespace dagbase
         using LookupTableFrom = VectorSet<SignalPath*, CompareSignalPathsByFrom>;
         using LookupTableTo = VectorSet<SignalPath*, CompareSignalPathsByTo>;
 
-        struct FindResult
+        struct DAGBASE_API FindResult
         {
             using value_type = SignalPath *;
             Variant find(std::string_view path) const;
