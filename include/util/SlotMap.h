f02744ae1b2ec70dd044d71ffdb3ce68c1b14b37
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-08-16 19:23:00 +0100

    test,data:SlotMapTest.cpp, AllocFree.lua:Add tests for allocating and deallocating on a SlotMap. build:dagbase/CMakeLists.txt:Add SlotMap to cmake build. feat:SlotMap.h:Add template supporting stable indices over a vector. - still invalidates references if alloc() causes the vector to grow.

---
+++ include/util/SlotMap.h
@@ -0,0 +1,151 @@
+//
+// Created by Tony Horrobin on 16/08/2026.
+//
+
+#pragma once
+
+#include "core/ConfigurationElement.h"
+#include "util/Searchable.h"
+
+#include <cstdint>
+#include <vector>
+#include <string_view>
+#include <iostream>
+
+namespace dagbase
+{
+    template<typename T>
+    class SlotMap
+    {
+    public:
+        struct Ident
+        {
+            static constexpr std::uint32_t INVALID_INDEX = ~0U;
+
+            void configure(ConfigurationElement& config)
+            {
+                ConfigurationElement::readConfig(config, "index", &index);
+                ConfigurationElement::readConfig(config, "gen", &gen);
+            }
+
+            bool operator==(const Ident& other) const
+            {
+                return index == other.index && gen == other.gen;
+            }
+
+            friend
+            std::ostream& operator<<(std::ostream& str, const typename SlotMap<T>::Ident& value)
+            {
+                str << "SlotMap.Ident { index: " << value.index << ", gen: " << value.gen << " }";
+
+                return str;
+            }
+
+            std::uint32_t index{INVALID_INDEX};
+            std::uint32_t gen{0};
+        };
+
+        struct Item
+        {
+            T item;
+            Ident id;
+        };
+
+        explicit SlotMap(std::size_t count)
+        {
+            _data.reserve(count);
+        }
+
+        T& alloc()
+        {
+            if (_freeHead == Ident::INVALID_INDEX)
+            {
+                _data.emplace_back();
+                _data.back().id.index = _data.size()-1;
+                _data.back().id.gen = _nextGen;
+                ++_size;
+                return _data.back().item;
+            }
+            auto lastIndex = _data[_freeHead].id.index;
+            _data[_freeHead].id.gen = _nextGen;
+            _data[_freeHead].id.index = _size;
+            _freeHead = lastIndex;
+            return _data[_size++].item;
+        }
+
+        void free(T& value)
+        {
+            Item* item = (Item*)&value;
+
+            if (_freeHead == Ident::INVALID_INDEX)
+            {
+                _freeHead = item->id.index;
+                item->id.index = Ident::INVALID_INDEX;
+            }
+            else
+            {
+                auto oldFreeHead = _freeHead;
+                _freeHead = item->id.index;
+                item->id.index = oldFreeHead;
+
+            }
+
+            item->id.gen = 0;
+            ++_nextGen;
+            --_size;
+        }
+
+        Ident id(T& value)
+        {
+            Item * item = (Item*)&value;
+
+            return item->id;
+        }
+
+        T& get(Ident id)
+        {
+            return _data[id.index].item;
+        }
+
+        T* tryGet(Ident id)
+        {
+            if (id.index<_data.size() &&_data[id.index].id.gen == id.gen)
+            {
+                return &_data[id.index].item;
+            }
+
+            return nullptr;
+        }
+
+        std::uint32_t size() const
+        {
+            return _size;
+        }
+
+        Variant find(std::string_view path) const
+        {
+            Variant retval;
+
+            retval = findEndpoint(path, "size", size());
+            if (retval.has_value())
+                return retval;
+
+            retval = findEndpoint(path, "nextGen", _nextGen);
+            if (retval.has_value())
+                return retval;
+
+            retval = findEndpoint(path, "freeHead", _freeHead);
+            if (retval.has_value())
+                return retval;
+
+            return {};
+        }
+    private:
+        using Array = std::vector<Item>;
+        Array _data;
+        std::uint32_t _size{0};
+        std::uint32_t _nextGen{1};
+        std::uint32_t _freeHead{Ident::INVALID_INDEX};
+    };
+
+}
---
+++ src/apps/DagBaseTest/SlotMapTest.cpp
@@ -0,0 +1,139 @@
+//
+// Created by Tony Horrobin on 16/08/2026.
+//
+
+#include <gtest/gtest.h>
+
+#include "util/SlotMap.h"
+#include "core/ConfigurationElement.h"
+#include "core/LuaInterface.h"
+#include "test/TestUtils.h"
+#include "util/enums.h"
+
+struct SlotMapScriptItem
+{
+    enum Command : std::uint32_t
+    {
+        COMMAND_UNKNOWN,
+        COMMAND_ALLOC,
+        COMMAND_FREE,
+        COMMAND_TRY_GET
+    };
+
+    Command cmd{COMMAND_UNKNOWN};
+
+    using Sut = dagbase::SlotMap<int>;
+    Sut::Ident id;
+    using Assertions = std::vector<Assertion<dagbase::SlotMap<int>>>;
+    Assertions assertions;
+
+    void configure(dagbase::ConfigurationElement& config)
+    {
+        dagbase::ConfigurationElement::readConfig<Command>(config, "cmd", &parseCommand, &cmd);
+        dagbase::ConfigurationElement::readConfig(config, "id", &id);
+        dagbase::ConfigurationElement::readConfigVector(config, "assertions", &assertions);
+    }
+
+    void makeItSo(Sut& sut)
+    {
+        switch (cmd)
+        {
+            case COMMAND_ALLOC:
+            {
+                auto& item = sut.alloc();
+                auto actual = sut.id(item);
+                ASSERT_EQ(id, actual);
+
+                break;
+            }
+            case COMMAND_FREE:
+            {
+                sut.free(sut.get(id));
+                ASSERT_EQ(nullptr, sut.tryGet(id));
+
+                break;
+            }
+            default:
+                FAIL() << "Got into unhandled command " << commandToString(cmd);
+
+                break;
+        }
+
+        for (auto& a : assertions)
+            a.makeItSo(sut, commandToString(cmd));
+    }
+
+    static const char* commandToString(Command value)
+    {
+        switch (value)
+        {
+            ENUM_NAME(COMMAND_UNKNOWN)
+            ENUM_NAME(COMMAND_ALLOC)
+            ENUM_NAME(COMMAND_FREE)
+            ENUM_NAME(COMMAND_TRY_GET)
+        }
+
+        return "<error>";
+    }
+
+    static Command parseCommand(const char* str)
+    {
+        TEST_ENUM(COMMAND_UNKNOWN, str);
+        TEST_ENUM(COMMAND_ALLOC, str);
+        TEST_ENUM(COMMAND_FREE, str);
+        TEST_ENUM(COMMAND_TRY_GET, str);
+
+        return COMMAND_UNKNOWN;
+    }
+};
+
+struct SlotMapScript
+{
+    using Array = std::vector<SlotMapScriptItem>;
+    Array items;
+
+    void configure(dagbase::ConfigurationElement& config)
+    {
+        dagbase::ConfigurationElement::readConfigVector(config, "items", &items);
+    }
+
+    void makeItSo(SlotMapScriptItem::Sut& sut)
+    {
+        for (auto item : items)
+        {
+            item.makeItSo(sut);
+        }
+    }
+};
+
+class SlotMap_testScripted : public ::testing::TestWithParam<std::tuple<const char*>>
+{
+protected:
+    void SetUp() override
+    {
+        _sut = new SlotMapScriptItem::Sut(10);
+    }
+
+    void TearDown() override
+    {
+        delete _sut;
+    }
+
+    SlotMapScript _script;
+    SlotMapScriptItem::Sut* _sut{nullptr};
+};
+
+TEST_P(SlotMap_testScripted, testExpectedValue)
+{
+    auto filename = std::get<0>(GetParam());
+    dagbase::Lua lua;
+    auto config = dagbase::ConfigurationElement::fromFile(lua, filename);
+    ASSERT_NE(nullptr, config);
+    _script.configure(*config);
+    _script.makeItSo(*_sut);
+    delete config;
+}
+
+INSTANTIATE_TEST_SUITE_P(SlotMap, SlotMap_testScripted, ::testing::Values(
+    std::make_tuple("data/tests/SlotMap/AllocAndFree.lua")
+    ));
